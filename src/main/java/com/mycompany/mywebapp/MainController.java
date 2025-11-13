44fea52f7839844c15fab8574bd19014e86cefcc
sandersoovli <sander.soovali@voco.ee>
2025-11-13 11:13:16 +0200

    Home Page

---
+++ src/main/java/com/mycompany/mywebapp/MainController.java
@@ -0,0 +1,14 @@
+package com.mycompany.mywebapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("")
+    public String showHomePage(){
+        System.out.println("HomePage");
+        return "index";
+    }
+}
