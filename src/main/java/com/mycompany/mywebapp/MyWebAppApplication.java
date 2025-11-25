a1877963c951018335785d0f96cc53ccbbdb5fe8
sandersoovli <sander.soovali@voco.ee>
2025-11-25 15:31:53 +0200

    tomcat deployment war file

--- src/main/java/com/mycompany/mywebapp/MyWebAppApplication.java
+++ src/main/java/com/mycompany/mywebapp/MyWebAppApplication.java
@@ -2,12 +2,18 @@ package com.mycompany.mywebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyWebAppApplication {
+public class MyWebAppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MyWebAppApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MyWebAppApplication.class);
     }
 
 }
